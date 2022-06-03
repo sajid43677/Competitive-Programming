06f8f062fd1ce8e2c28598ce1be62fa07084447f
sajid43677 <20-43677-2@student.aiub.edu>
2022-06-03 23:12:32 +0600

    2

---
+++ A_Beat_The_Odds.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+
+//double pi = acos(-1.0);
+
+#define allZero(a) memset(a,0,sizeof(a));
+#define allnegOne(a) memset(a,-1,sizeof(a));
+#define endl "\n"
+#define Boost ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define reverseS string(curr.rbegin(),curr.rend()); //reverse string assignment
+#define Read freopen("input.txt","r",stdin);
+#define Write freopen("output.txt","w",stdout);
+#define pb(x) push_back(x);
+#define for1(n) for(int i = 0; i < n;i++)
+
+void solve(){
+    int n;
+    cin >> n;
+    int a = 0,prev = 0;
+    int cnt = 0;
+    cin >> prev;
+    for1(n-1){
+        cin >> a;
+        if((a+prev)%2 != 0){
+            cnt++;
+        }
+        else{
+            prev = a;
+        }
+    }
+    cout << cnt << endl;
+}
+
+//FuzzyCarnage
+int main()
+{
+    Boost;
+    //Read;Write;
+    int t;
+    cin >> t;
+    while(t--) 
+        solve();
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
